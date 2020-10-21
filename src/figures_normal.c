59d60eadaf6f5ec0ce618b8d9f8402255dda086f
ale_xxx <vasyekk67@yandex.ru>
2020-10-21 13:36:17 +0300

    cone fixed

--- src/figures_normal.c
+++ src/figures_normal.c
@@ -21,19 +21,29 @@ t_vec3		cone_normal(t_obj *obj, t_vec3 *d, t_num min_dist, t_cam cam)
 	t_vec3	v;
 	t_vec3	oc;
 	t_num	m;
-	t_num	k = ((t_cone*)obj)->tg;
+	t_num	k = tan((((t_cone*)obj)->tg));
 	t_vec3	p;
 	t_vec3	p_c;
 	t_vec3	v_m;
 
 	v = vec3_normalize(((t_cone*)obj)->dir_vec);
 	oc = vec3_minus(cam.pos, ((t_cone*)obj)->coords);
-	m = vec3_dot(*d, vec3_mult_num(v, min_dist)) + vec3_dot(oc, v);
+//	m = vec3_dot(*d, vec3_mult_num(v, min_dist)) + vec3_dot(oc, v);
+	m = vec3_dot(*d, v) * min_dist + vec3_dot(oc, v);
 	v_m = vec3_mult_num(v, m);
-	p = vec3_plus(vec3_mult_num(*d, min_dist), cam.pos);
+	p = vec3_plus(cam.pos, vec3_mult_num(*d, min_dist));
 	p_c = vec3_minus(p, ((t_cone*)obj)->coords);
-	return vec3_normalize(vec3_minus(vec3_minus(v_m, p_c), vec3_mult_num(v, m*k*k)));
+//	return vec3_normalize(vec3_minus(vec3_minus(v_m, p_c), vec3_mult_num(v, m)));
+	return vec3_normalize(vec3_minus(vec3_mult_num(v, m*(1+k*k)), p_c));//tan(tg)^2 + 1 == 1/cos(tg)^2
+	//{-r*cos(angle), -r*sin(angle), r}d(r)d(angle)
+	//vec3_minus(vec3_plus(), vec3_mult_num())
+//	mv_minus(mv_plus(mv_mult_num(ray=v, intersect_dist=m), oc), mv_mult_num(obj.axis, (1 + k2) * m));
 }
+
+//m = vec3_dot(r.dir, cylinder.v) * t 'min_dist' + vec_dot(vec_minus(cam, cylinder.pos), cylinder.v);+++++++++++
+//p = vec_add(r.start, vec_scale(r.dir, t));++++
+//n = vec_norm(vec_sub(vec_sub(p, cylinder.pos), vec_scale(cylinder.v, m)));
+
 //t_vec3		cylinder_normal(t_obj *obj, t_vec3 *d, t_num min_dist, t_cam cam)
 //{
 //	t_vec3	v;
